55da31525dcf4ddc9ee07b0a53a28ddb019e2212
LepkoQQ <lepko.san@gmail.com>
2022-08-29 19:06:13 +0200

    new footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="content-wrapper">
     <div class="content">
-      <router-view/>
+      <router-view />
     </div>
-    <footer>
-      <div class="left-side">
-        <p><a href="/about">{{ $t('footer.about') }}</a><a href="/howto">{{ $t('footer.howto') }}</a></p>
-        <a href="https://zainproti.si/" target="_blank"><img src="./assets/ZiP_logo.png" alt="ZiP logo"></a>
-        <a href="http://hdd.hr/" target="_blank"><img src="./assets/HDD_logo.png" alt="HDD logo"></a>
-        <a href="https://idebate.org/" target="_blank"><img src="./assets/IDEA_logo.png" alt="IDEA logo"></a>
-      </div>
-      <img src="./assets/EU_logo.png" alt="EU logo">
-    </footer>
+    <Footer />
   </div>
 </template>
 
+<script>
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
+
 <style lang="scss">
-@import 'src/index.scss';
+@import "src/index.scss";
 
 a:link {
-    color: #252525;
+  color: #252525;
 }
 
 h1 {
@@ -31,7 +33,7 @@ h1 {
   position: relative;
   display: inline-block;
   &::before {
-    content: '';
+    content: "";
     display: block;
     position: absolute;
     bottom: 15px;
@@ -39,74 +41,6 @@ h1 {
     width: 100%;
     height: 16px;
     background-color: #ffcc00;
-  }
-}
-
-footer {
-  flex-shrink: 0;
-  background: #ffffff;
-  border-top: 1px solid #3098f3;
-  padding: 8px 16px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  img {
-    max-height: 40px;
-    margin-right: 40px;
-    margin-bottom: 10px;
-
-    @media (min-width: 576px) {
-      margin: 0;
-    }
-
-    @media (min-width: 992px) {
-      margin-left: 40px;
-      max-height: 60px;
-    }
-  }
-
-  .left-side {
-    margin: 10px 0;
-
-    img {
-      margin: 10px;
-
-      @media (min-width: 576px) {
-        margin: 0 20px 0 0;
-      }
-
-      @media (min-width: 768px) {
-        margin-right: 20px;
-      }
-
-      @media (min-width: 992px) {
-        margin-left: 40px;
-        margin-right: 0;
-      }
-    }
-  }
-
-  @media (min-width: 576px) {
-    padding: 15px 30px;
-    justify-content: space-between;
-    flex-direction: row;
-  }
-
-  p {
-    color: #252525;
-    font-family: Poppins;
-    font-size: 16px;
-    font-weight: 300;
-    line-height: 60px;
-    margin: 0;
-    float: left;
-    display: none;
-
-    @media (min-width: 992px) {
-      display: block;
-    }
   }
 }
 
@@ -144,5 +78,4 @@ footer {
     display: none !important;
   }
 }
-
 </style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <footer>
+    <div class="links">
+      <a href="/about">{{ $t("footer.about") }}</a>
+      <a href="/howto">{{ $t("footer.howto") }}</a>
+    </div>
+    <div class="divider"></div>
+    <div class="logos">
+      <div class="left-side">
+        <a href="https://zainproti.si/" target="_blank"
+          ><img src="../assets/ZiP_logo.png" alt="ZiP logo"
+        /></a>
+        <a href="http://hdd.hr/" target="_blank"
+          ><img src="../assets/HDD_logo.png" alt="HDD logo"
+        /></a>
+        <a href="https://idebate.org/" target="_blank"
+          ><img src="../assets/IDEA_logo.png" alt="IDEA logo"
+        /></a>
+      </div>
+      <div class="right-side">
+        <img src="../assets/EU_logo.png" alt="EU logo" />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  padding-inline: 60px;
+
+  .links {
+    display: flex;
+    gap: 100px;
+    padding: 20px;
+    font-family: "Inter", sans-serif;
+    font-size: 16px;
+    font-weight: 600;
+    color: #252525;
+
+    a,
+    a:visited {
+      color: inherit;
+      text-decoration: none;
+      border-bottom: 1px solid #000;
+    }
+  }
+
+  .divider {
+    border-bottom: 2px solid #000;
+  }
+
+  .logos {
+    display: flex;
+    justify-content: space-between;
+    padding: 16px 20px;
+
+    .left-side,
+    .right-side {
+      display: flex;
+      gap: 40px;
+    }
+
+    img {
+      height: 45px;
+    }
+  }
+}
+</style>
