c35a07fce4b0f97c8669d63bc7669d4633d2646e
LepkoQQ <lepko.san@gmail.com>
2022-08-29 15:20:40 +0200

    style lesson view

--- frontend/src/components/Lesson.vue
+++ frontend/src/components/Lesson.vue
@@ -1,64 +1,92 @@
 <template>
-  <div class="parentContainer">
-    <h1>{{ lesson.title }}</h1>
-    <ul class="tags">
-      <li class="tag" v-for="topic in lesson.topic" :key="topic">
-        <span class="tag-text">{{ topic }}</span>
-      </li>
-    </ul>
-    <span class="lesson-text">{{ lesson.description }}</span>
-    <div class="line"></div>
-    <button class="download">DOWNLOAD</button>
-    <div class="info">
-      <span class="meta">Material type</span>
-      <span
-        class="value"
-        v-for="materialType in lesson.material_type"
-        :key="materialType"
-      >{{ materialType }}</span>
+  <div class="parent-container">
+    <div class="main-content">
+      <h1>{{ lesson.title }}</h1>
+      <ul class="tags">
+        <li class="tag" v-for="topic in lesson.topic" :key="topic">
+          <span class="tag-text">{{ topic }}</span>
+        </li>
+      </ul>
+      <div class="lesson-text">{{ lesson.description }}</div>
     </div>
-    <div class="info">
-      <span class="meta">Language</span>
-      <span class="value" v-for="language in lesson.language" :key="language">{{ language }}</span>
-    </div>
-    <div class="info">
-      <span class="meta">Topic</span>
-      <span class="value" v-for="topic in lesson.topic" :key="topic">{{ topic }}</span>
-    </div>
-    <div class="info">
-      <span class="meta">Age and difficulty</span>
-      <span
-        class="value"
-        v-for="ageAndDifficulty in lesson.age_and_difficulty"
-        :key="ageAndDifficulty"
-      >{{ ageAndDifficulty }}</span>
-    </div>
-    <div class="info">
-      <span class="meta">Duration</span>
-      <span class="value" v-for="duration in lesson.duration" :key="duration">{{ duration }}</span>
-    </div>
-    <div class="info">
-      <span class="meta">Activity type</span>
-      <span
-        class="value"
-        v-for="activityType in lesson.activity_type"
-        :key="activityType"
-      >{{ activityType }}</span>
-    </div>
-    <div class="info">
-      <span class="meta">Similar lessons</span>
-      <a
-        class="value"
-        v-for="similarLesson in lesson.similar_lessons"
-        :key="similarLesson"
-        :href="`/lesson/{{ lesson.id }}`"
-      >{{ lesson.title }}</a>
+    <div class="sidebar">
+      <button class="download">
+        Download lesson
+        <img src="../assets/download.svg" />
+      </button>
+      <div class="infos">
+        <div class="info">
+          <span class="meta">Material type</span>
+          <span
+            class="value"
+            v-for="materialType in lesson.material_type"
+            :key="materialType"
+            >{{ materialType }}</span
+          >
+        </div>
+        <div class="info">
+          <span class="meta">Language</span>
+          <span
+            class="value"
+            v-for="language in lesson.language"
+            :key="language"
+            >{{ language }}</span
+          >
+        </div>
+        <div class="info">
+          <span class="meta">Topic</span>
+          <span class="value" v-for="topic in lesson.topic" :key="topic">{{
+            topic
+          }}</span>
+        </div>
+        <div class="info">
+          <span class="meta">Age and difficulty</span>
+          <span
+            class="value"
+            v-for="ageAndDifficulty in lesson.age_and_difficulty"
+            :key="ageAndDifficulty"
+            >{{ ageAndDifficulty }}</span
+          >
+        </div>
+        <div class="info">
+          <span class="meta">Duration</span>
+          <span
+            class="value"
+            v-for="duration in lesson.duration"
+            :key="duration"
+            >{{ duration }}</span
+          >
+        </div>
+        <div class="info">
+          <span class="meta">Activity type</span>
+          <span
+            class="value"
+            v-for="activityType in lesson.activity_type"
+            :key="activityType"
+            >{{ activityType }}</span
+          >
+        </div>
+        <div class="info similar-lessons">
+          <span class="meta">Similar lessons</span>
+          <div class="values">
+            <div
+              v-for="similarLesson in lesson.similar_lessons"
+              :key="similarLesson"
+              class="value"
+            >
+              <a :href="`/lesson/${similarLesson.id}`">{{
+                similarLesson.title
+              }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import LessonList from './LessonList.vue'
+import LessonList from "./LessonList.vue";
 
 export default {
   data() {
@@ -66,102 +94,180 @@ export default {
       isAuth: false,
       languagesDictionary: {},
       tags: null,
-      lessons: []
-    }
+      lessons: [],
+    };
   },
   components: {
-    LessonList
+    LessonList,
   },
-  props: ['id', 'lesson'],
+  props: ["id", "lesson"],
   methods: {
     async getFilteredLessons(language) {
-      this.$router.push({ name: 'Home', params: { language: [this.languagesDictionary[language], language] } })
-    }
+      this.$router.push({
+        name: "Home",
+        params: { language: [this.languagesDictionary[language], language] },
+      });
+    },
   },
   removeFilter() {
     this.tags = null;
   },
   async created() {
-    const languageArray = ['SL', 'EN']
-    languageArray.forEach(language => {
-      this.languagesDictionary[language.id] = language.value
+    const languageArray = ["SL", "EN"];
+    languageArray.forEach((language) => {
+      this.languagesDictionary[language.id] = language.value;
     });
-  }
-}
-
+  },
+};
 </script>
 
 <style scoped lang="scss">
-.parentContainer {
+.parent-container {
   display: flex;
-  flex-direction: column;
-  background-color: #ffffff;
-  padding: 20px;
-  margin-top: 100px;
+  gap: 50px;
+  margin-top: 50px;
   margin-bottom: 40px;
 
-  @media (min-width: 992px) {
-    padding: 40px;
-  }
+  .main-content {
+    flex: 2;
 
-  p {
-    font-family: "IBM Plex Mono";
-    font-size: 14px;
-    margin-bottom: 10px;
-    margin-top: 40px;
+    h1 {
+      margin: 0;
+      color: #252525;
+      font-family: "Fredoka", sans-serif;
+      font-weight: 600;
+      font-size: 46px;
+    }
 
-    @media (min-width: 992px) {
-      font-size: 18px;
+    .tags {
+      list-style: none;
+      margin: 10px 0 0 0;
+      padding: 0;
+
+      .tag {
+        display: inline-block;
+        background-color: #c1ead7;
+        padding: 6px 14px;
+
+        & + .tag {
+          margin-left: 14px;
+        }
+
+        .tag-text {
+          color: #252525;
+          font-family: "Inter", sans-serif;
+          font-size: 18px;
+        }
+      }
+    }
+
+    .lesson-text {
+      margin-top: 48px;
+      color: #252525;
+      font-family: "Inter", sans-serif;
+      font-size: 21px;
+      line-height: 31px;
     }
   }
-}
-.lessonButtons {
-  display: flex;
-  justify-content: space-between;
-}
-.line {
-  margin-top: 26px;
-  border-top: 1px solid;
-}
-.tags {
-  list-style: none;
-  margin-top: 20px;
-  overflow: hidden;
-  padding: 0;
-  cursor: pointer;
-}
-.tag {
-  display: inline-block;
-  padding-left: 10px;
-  padding-right: 10px;
-  margin: 10px;
-  border-radius: 14px;
-  border: 2px solid #3098f3;
-  &.selected {
-    background-color: #3098f3;
-  }
-}
-.tag-text {
-  color: #000000;
-  font-family: "IBM Plex Mono";
-  font-size: 14px;
-  font-weight: 400;
-  font-style: italic;
-  letter-spacing: normal;
-  line-height: 18px;
-  text-align: center;
-}
 
-.lesson-text {
-  /* Style for "EU member" */
-  color: #252525;
-  font-family: Poppins;
-  font-size: 32px;
-  line-height: 48px;
+  .sidebar {
+    flex: 1;
 
-  @media (min-width: 992px) {
-    font-size: 48px;
-    line-height: 60px;
+    .download {
+      position: relative;
+      display: flex;
+      align-items: center;
+      gap: 23px;
+      padding: 16px 34px;
+      background: #fff;
+      border: 2px solid #000;
+      font-family: "Inter", sans-serif;
+      font-size: 18px;
+      font-weight: 600;
+      color: #252525;
+      cursor: pointer;
+      transform-style: preserve-3d;
+
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        inset: 0;
+        border: 2px solid #000;
+        background-color: #aefcd8;
+        transform: translate(9px, 9px) translateZ(-1px);
+      }
+
+      img {
+        width: 20px;
+        height: 25px;
+      }
+    }
+
+    .infos {
+      margin-top: 45px;
+
+      .info {
+        border: 1px solid #000;
+        padding: 18px;
+        width: fit-content;
+
+        & + .info {
+          margin-top: -1px;
+        }
+
+        .meta {
+          color: #252525;
+          font-family: "Inter", sans-serif;
+          font-size: 14px;
+          font-style: italic;
+          margin-right: 18px;
+        }
+
+        .value {
+          color: #252525;
+          font-family: "Inter", sans-serif;
+          font-size: 18px;
+          font-weight: 600;
+        }
+
+        &.similar-lessons {
+          width: auto;
+          display: flex;
+
+          .meta {
+            width: min-content;
+          }
+
+          .value {
+            & + .value {
+              margin-top: 14px;
+            }
+
+            a {
+              &,
+              &:visited {
+                display: inline;
+                font-size: 16px;
+                text-decoration: none;
+                color: #252525;
+                border-bottom: 1px solid #000;
+              }
+
+              &::before {
+                content: "";
+                display: inline-block;
+                background-image: url("../assets/open-new-page.svg");
+                background-repeat: no-repeat;
+                width: 14px;
+                height: 14px;
+                margin-right: 5px;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
--- frontend/src/views/LessonView.vue
+++ frontend/src/views/LessonView.vue
@@ -3,7 +3,7 @@
     <div class="container-child">
       <div class="wrapper">
         <div class="debate-logo"></div>
-        <router-link to="/">Back to the list</router-link>
+        <router-link to="/" class="back-link">Back to the list</router-link>
         <lesson class="lesson" :lesson="lesson" :id="id" />
       </div>
     </div>
@@ -11,27 +11,54 @@
 </template>
 
 <script>
-import Lesson from '../components/Lesson.vue'
+import Lesson from "../components/Lesson.vue";
 
 export default {
   data() {
     return {
       lesson: {},
-      }
-    },
+    };
+  },
   components: {
     Lesson,
-    },
-  props: ['id'],
+  },
+  props: ["id"],
   async created() {
-    this.lesson = await this.$store.dispatch('getLesson', {id: this.id})
-    this.$store.state.lessons.lesson = this.lesson
-    const choice = this.votes.find(vote => vote.lesson === this.lesson.id)
+    this.lesson = await this.$store.dispatch("getLesson", { id: this.id });
+    this.$store.state.lessons.lesson = this.lesson;
+    const choice = this.votes.find((vote) => vote.lesson === this.lesson.id);
     if (choice) this.lesson.choice = choice.choices;
-  }
-}
-
+  },
+};
 </script>
 
 <style scoped lang="scss">
+.container-child {
+  background-color: #f1f2f2;
+
+  .wrapper {
+    max-width: 1300px;
+    margin-inline: auto;
+
+    .back-link {
+      font-family: "Inter", sans-serif;
+      font-size: 14px;
+      text-decoration: none;
+      color: #252525;
+
+      &:before {
+        content: '';
+        display: inline-block;
+        width: 18px;
+        height: 10px;
+        background-image: url("../assets/back-arrow.svg");
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: 31px;
+        transform: rotate(180deg);
+        margin-right: 10px;
+      }
+    }
+  }
+}
 </style>
